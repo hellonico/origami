c826ff105c07ad36566ce8509402a28272136a30
Nicolas Modrzyk <hellonico@gmail.com>
2017-11-10 09:48:16 +0900

    project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject origami "0.1.0"
+(defproject origami "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+:profiles {:dev {
+    :resource-paths ["resources-dev"]
+    :dependencies [
+    ; used for api code only
+    [camel-snake-kebab "0.4.0"]]}}            
   :dependencies [
   	[org.clojure/clojure "1.8.0"]
     [opencv/opencv "3.3.1"]
