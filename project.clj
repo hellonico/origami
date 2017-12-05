e6efa7eb4e3e22895cbec8dedebdc78a082c9277
Nicolas Modrzyk <nico@karabiner-software.com>
2017-12-05 17:29:11 +0900

    preparation for release

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 :repositories [
 ["vendredi" {:url "https://repository.hellonico.info/repository/hellonico/" :creds :gpg}]
-["releases" {:url "https://repository.hellonico.info/repository/hellonico/" :creds :gpg}]
 ]
 :resource-paths ["rsc"]
 :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
