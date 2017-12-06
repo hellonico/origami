39f7db090c959cd1766b54103b821dd851459ece
Nicolas Modrzyk <nico@karabiner-software.com>
2017-12-06 10:28:57 +0900

    Version 0.1.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject origami "0.1.10"
+(defproject origami "0.1.11-SNAPSHOT"
   :description "OpenCV Wrapper"
   :url "https://github.com/hellonico/origami"
   :license {:name "Eclipse Public License"
