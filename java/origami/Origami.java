aa525a8fdcee7af34fa1756519631939dc533b11
Nicolas Modrzyk <hellonico@gmail.com>
2019-08-16 11:23:52 +0900

    adding url-bi-mat conversion

--- java/origami/Origami.java
+++ java/origami/Origami.java
@@ -3,7 +3,22 @@ package origami;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.scijava.nativelib.NativeLoader;
 
 public class Origami {
 
@@ -21,4 +36,45 @@ public class Origami {
         frame.get(0, 0, data);
         return image;
     }
+
+    public static Mat bufferedImagetoMat(BufferedImage image) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", byteArrayOutputStream);
+        byteArrayOutputStream.flush();
+        return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()), Imgcodecs.IMREAD_UNCHANGED);
+    }
+
+    private static final int BUFFER_SIZE = 8192;
+
+    private static long copy(InputStream source, OutputStream sink) throws IOException {
+        long nread = 0L;
+        byte[] buf = new byte[BUFFER_SIZE];
+        int n;
+        while ((n = source.read(buf)) > 0) {
+            sink.write(buf, 0, n);
+            nread += n;
+        }
+        return nread;
+    }
+
+    public static Mat urlToMat(String url) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream in = new URL(url).openStream()) {
+            copy(in, byteArrayOutputStream);
+        }
+        byteArrayOutputStream.flush();
+        return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()), Imgcodecs.IMREAD_UNCHANGED);
+    }
+    /**
+     * Usage:
+     * <pre>
+     * public static void main(String[] args) throws IOException {
+     *     NativeLoader.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+     *     Mat mat1 = urlToMat("https://raw.githubusercontent.com/hellonico/origami/master/doc/origami.jpg");
+     *     BufferedImage bi = matToBufferedImage(mat1);
+     *     Mat mat2 = bufferedImagetoMat(bi);
+     *     Imgcodecs.imwrite("origami.jpg", mat2);
+     * }
+     * </pre>
+     **/
 }
