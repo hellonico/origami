8bcd2967bc10883309e0a88a8cab7568ce701944
Nicolas Modrzyk <hellonico@gmail.com>
2020-03-05 18:48:56 +0900

    clojure based filter

---
+++ java/origami/filter/ClojureFilter.java
@@ -0,0 +1,46 @@
+package origami.filter;
+
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+import clojure.lang.PersistentList;
+import clojure.lang.Symbol;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import origami.Filter;
+import origami.Origami;
+
+import static clojure.java.api.Clojure.var;
+
+public class ClojureFilter implements Filter {
+    static IFn EDN_READ_STRING;
+
+    static {
+        Symbol edn = (Symbol) var("clojure.core", "symbol").invoke("clojure.edn");
+        Clojure.var("clojure.core", "require").invoke(edn);
+        EDN_READ_STRING = var("clojure.edn", "read-string");
+    }
+
+    IFn eval = Clojure.var("clojure.core", "eval");
+    IFn myfn;
+    String myfnString;
+
+    public ClojureFilter() {
+        this.setFn("(fn[mat] mat)");
+    }
+
+    public void setFn(String text) {
+        this.myfnString = text;
+        this.myfn = (IFn) eval.invoke(EDN_READ_STRING.invoke(text));
+    }
+
+    public String getFn() {
+        return this.myfnString;
+    }
+
+
+    @Override
+    public Mat apply(Mat mat) {
+        return (Mat) myfn.invoke(mat);
+    }
+
+}
--- test-java/coffee/FilterTest.java
+++ test-java/coffee/FilterTest.java
@@ -1,12 +1,16 @@
 package coffee;
 
+import clojure.lang.PersistentList;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import origami.Filter;
 import origami.Filters;
 import origami.Origami;
+import origami.filter.ClojureFilter;
 import origami.filter.NoOPFilter;
 
 import java.io.File;
@@ -42,6 +46,17 @@ public class FilterTest {
         File f = new File(ClassLoader.getSystemResource("noop.edn").getFile());
         Filter noopf = StringToFilter(f);
         Assert.assertEquals(noopf.getClass(), NoOPFilter.class);
+    }
+
+    @Test
+    public void clojureFilter() {
+            Origami.init();
+            String function = "(fn[mat] (org.opencv.imgproc.Imgproc/applyColorMap mat mat 4) mat)";
+            ClojureFilter cf = new ClojureFilter();
+            cf.setFn(function);
+            Mat me = Imgcodecs.imread("doc/lena.png");
+            Mat me2 = cf.apply(me);
+            Imgcodecs.imwrite("target/me2.png", me2);
     }
 
 }
