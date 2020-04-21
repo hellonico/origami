913ba3f7d10600a9291f2e9a46b3047afb547d77
Nicolas Modrzyk <nico@karabiner-software.com>
2020-04-21 13:40:15 +0900

    Version 4.3.0-4

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
       ; _ (println (str platform mxnet))
       ]
 
-(defproject origami "4.3.0-4-SNAPSHOT"
+(defproject origami "4.3.0-4"
   :description "OpenCV4 Wrapper"
   :aot [opencv4.dnn.core opencv4.filter opencv4.video]
   :url "https://github.com/hellonico/origami"
