afde119a72febfb35f6e3c80ab6ebf517132aa7f
Nicolas Modrzyk <hellonico@gmail.com>
2020-10-20 11:58:37 +0900

    Version 4.5.0

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
       ; _ (println (str platform mxnet))
       ]
 
-  (defproject origami "4.5.0-0"
+  (defproject origami "4.5.0"
     :description "OpenCV4 Wrapper"
     :aot [opencv4.dnn.core]
     :url "https://github.com/hellonico/origami"
