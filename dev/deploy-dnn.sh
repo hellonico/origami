8499790192f18d1c695c7f75ea857ee32f9b38ce
Nicolas Modrzyk <hellonico@gmail.com>
2023-05-08 10:21:19 +0900

    Add script to deploy networks #95

--- dev/deploy-dnn.sh
+++ dev/deploy-dnn.sh
@@ -1,16 +1,34 @@
 #!/bin/zsh
 
-DIR=$HOME/.origami/yolov6n-1.0.0.zip
+# > tree yolov6t-1.0.zip/                                                                                                                                           (base) 09:12:31
+# yolov6t-1.0.zip/
+# ├── coco.names
+# └── yolov6t_v1.onnx
+
+DIR=$1
 PARENT="$(dirname $DIR)"
 FILE="$(basename $DIR)"
-(cd $PARENT && zip -r - $FILE) > deploy.zip
+NETWORK=`echo $FILE | cut -d"-" -f1`
+VERSION=`echo $FILE | cut -d"-" -f2 | rev | cut -d"." -f2- | rev`
+TEMPFILE=deploy.zip
 
+rm $DIR/**/.DS_Store
+
+echo "Preparing: $NETWORK $VERSION from $FILE located in $PARENT"
+echo "Waiting 5 seconds ..."
+sleep 5
+
+
+
+(cd $PARENT && zip -r - $FILE) >$TEMPFILE
+
+echo "Uploading"
 mvn deploy:deploy-file -DgroupId=networks.yolo \
-  -DartifactId=yolov6n \
-  -Dversion=1.0 \
+  -DartifactId=$NETWORK \
+  -Dversion=$VERSION \
   -Dpackaging=zip \
-  -Dfile=deploy.zip \
+  -Dfile=$TEMPFILE \
   -DrepositoryId=vendredi \
   -Durl=https://repository.hellonico.info/repository/hellonico/
 
-rm deploy.zip
+rm $TEMPFILE
