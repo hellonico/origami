7f36b0986330676de8b39e4da15d77c18c00b316
Nicolas Modrzyk <hellonico@gmail.com>
2023-05-08 13:30:13 +0900

    update script for uploading networks yolov6

--- dev/create-dnn-folder.sh
+++ dev/create-dnn-folder.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-VERSION="3.0"
+VERSION="3.1"
+EXTRA="_face"
+#EXTRA=""
 BASE="https://github.com/meituan/YOLOv6/releases/download/0.$VERSION"
 
 for value in n l s m
 do
-   URL=$BASE/yolov6$value.onnx
-   TARGET=yolov6$value-$VERSION
+   NAME=yolov6$value$EXTRA
+   URL=$BASE/$NAME.onnx
+   TARGET=$NAME-$VERSION
    TARGET_FOLDER=/tmp/$TARGET.zip
    mkdir $TARGET_FOLDER
    echo $URL >> $TARGET_FOLDER/get.url
